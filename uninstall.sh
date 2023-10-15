b3987ed8bf598aafc22ceebe9bcd36a0acc03e04
lxdklp <DAI200759@GMAIL.COM>
2023-10-15 17:00:29 +0800

    add files

---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+su
+rm -f /data/media/0/Android/data/com.RoamingStar.BlueArchive/files/AssetBundls/*
+rm -f /data/media/0/Android/data/com.RoamingStar.BlueArchive.bilibili/files/AssetBundls/*
